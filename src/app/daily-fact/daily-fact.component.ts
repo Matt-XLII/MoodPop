e1aa44af1a6ad9967b333cd466944a819f59b8da
Marcos Martins <marcos.martins@sncf.fr>
2023-06-08 12:06:14 +0200

    add css dailyfact

--- src/app/daily-fact/daily-fact.component.ts
+++ src/app/daily-fact/daily-fact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-daily-fact',
@@ -7,4 +7,52 @@ import { Component } from '@angular/core';
 })
 export class DailyFactComponent {
 
+  quote: string = "";
+
+  fetchQuoteFact() {    
+    fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
+      .then(response => response.json())
+      .then(quote => {
+        this.quote = quote.text;
+      });
+  }
+
+
+}*/
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-fact',
+  templateUrl: './daily-fact.component.html',
+  styleUrls: ['./daily-fact.component.scss']
+})
+export class DailyFactComponent implements OnInit {
+  quote: string = "";
+
+  ngOnInit() {
+    this.scheduleMidnightUpdate();
+    this.fetchQuoteFact();
+  }
+
+  fetchQuoteFact() {
+    fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
+      .then(response => response.json())
+      .then(quote => {
+        this.quote = quote.text;
+      });
+  }
+
+  scheduleMidnightUpdate() {
+    const now = new Date();
+    const midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0);
+    const timeUntilMidnight = midnight.getTime() - now.getTime();
+
+    setTimeout(() => {
+      this.fetchQuoteFact();
+      this.scheduleMidnightUpdate();
+    }, timeUntilMidnight);
+  }
 }
+
+
