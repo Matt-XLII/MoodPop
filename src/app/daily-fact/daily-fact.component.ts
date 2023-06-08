18afb211f97b074689b5a120d8b3246d00f4a890
Marcos Martins <marcos.martins@sncf.fr>
2023-06-08 15:08:18 +0200

    add css dailyfact + resolution prob bouton

--- src/app/daily-fact/daily-fact.component.ts
+++ src/app/daily-fact/daily-fact.component.ts
@@ -1,26 +1,6 @@
-/*import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-daily-fact',
-  templateUrl: './daily-fact.component.html',
-  styleUrls: ['./daily-fact.component.scss']
-})
-export class DailyFactComponent {
-
-  quote: string = "";
-
-  fetchQuoteFact() {    
-    fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
-      .then(response => response.json())
-      .then(quote => {
-        this.quote = quote.text;
-      });
-  }
 
 
-}*/
-
-import { Component, OnInit } from '@angular/core';
+/*import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-daily-fact',
@@ -53,6 +33,84 @@ export class DailyFactComponent implements OnInit {
       this.scheduleMidnightUpdate();
     }, timeUntilMidnight);
   }
+}*/
+
+/*--------------------------------------------
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-fact',
+  templateUrl: './daily-fact.component.html',
+  styleUrls: ['./daily-fact.component.scss']
+})
+export class DailyFactComponent implements OnInit {
+  quote: string = "";
+
+  ngOnInit() {
+    this.scheduleMidnightUpdate();
+    this.fetchQuoteFact();
+  }
+
+  fetchQuoteFact() {
+    fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
+      .then(response => response.json())
+      .then(quote => {
+        const limitedQuote = this.limitCharacterLength(quote.text, 130); 
+        this.quote = limitedQuote;
+      });
+  }
+
+  scheduleMidnightUpdate() {
+    const now = new Date();
+    const midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0);
+    const timeUntilMidnight = midnight.getTime() - now.getTime();
+
+    setTimeout(() => {
+      this.fetchQuoteFact();
+      this.scheduleMidnightUpdate();
+    }, timeUntilMidnight);
+  }
+
+  limitCharacterLength(text: string, maxLength: number): string {
+    if (text.length > maxLength) {
+      return text.substring(0, maxLength) + "...";
+    }
+    return text;
+  }
+}*/
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-fact',
+  templateUrl: './daily-fact.component.html',
+  styleUrls: ['./daily-fact.component.scss']
+})
+export class DailyFactComponent implements OnInit {
+  quote: string = "";
+
+  ngOnInit() {
+    this.fetchQuoteFact();
+  }
+
+  fetchQuoteFact() {
+    fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
+      .then(response => response.json())
+      .then(quote => {
+        const limitedQuote = this.limitCharacterLength(quote.text, 100); 
+        this.quote = limitedQuote;
+      });
+  }
+
+  limitCharacterLength(text: string, maxLength: number): string {
+    if (text.length > maxLength) {
+      return text.substring(0, maxLength) + "...";
+    }
+    return text;
+  }
 }
+
+
 
 
