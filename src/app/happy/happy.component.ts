6d8fa13b611487a436e55720ebbefda8911b3a0f
Rricardo2431 <128594768+Rricardo2431@users.noreply.github.com>
2023-06-08 12:06:24 +0200

    ajout du scss component happy

--- src/app/happy/happy.component.ts
+++ src/app/happy/happy.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class HappyComponent {
 
+
+joke : any = ""
+
+fetchData():void{
+  fetch("https://api.api-ninjas.com/v1/jokes?limit=1", {
+    method:'GET',
+    headers: {
+  'X-Api-key' : 'q80JOdvIGyOhfvSXFKWV4A==0y5PJl9FCDrqP2ik',
+  'Content-type': 'application/json',}})
+  .then(function (response) { return response.json()}) 
+  .then((data:any) => this.joke = data[0])
+ }
 }
+
+
+
