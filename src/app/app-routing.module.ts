123ed5b75c880fabe67c39332109ff24b4b4b931
Matt-XLII <durouchet.mathieu@gmail.com>
2023-06-13 16:26:36 +0200

    matching header and footer color + adding the base root to the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { DailyFactComponent } from './daily-fact/daily-fact.component';
 import { DailyMoodComponent } from './daily-mood/daily-mood.component';
 
 const routes: Routes = [
+
     { path: 'dailyfact', component: DailyFactComponent },
-    { path: 'dailymood', component: DailyMoodComponent}
+    { path: '', component: DailyMoodComponent}
 ];
 
 @NgModule({
