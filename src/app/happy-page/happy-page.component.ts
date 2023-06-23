38bcf3325d89972820254648537fae0df492c89c
tophe-w <128515611+tophe-w@users.noreply.github.com>
2023-06-23 16:13:00 +0200

    ajout des services

---
+++ src/app/happy-page/happy-page.component.ts
@@ -0,0 +1,22 @@
+import { Component,OnInit } from '@angular/core';
+import { HappyService } from '../services/happy.service';
+
+@Component({
+  selector: 'app-happy-page',
+  templateUrl: './happy-page.component.html',
+  styleUrls: ['./happy-page.component.scss']
+})
+export class HappyPageComponent implements OnInit {
+  jokes: object[] = [];
+
+constructor(private happyService: HappyService) {
+
+}
+
+ngOnInit(): void {
+this.happyService.fetchJokes(this.jokes);
+console.log(this.jokes);
+}
+
+
+}
