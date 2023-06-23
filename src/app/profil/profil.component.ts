6b1c06ce27e6e16ffc62dffe44f5c1ce8ffbe138
Mathieu Coulibaly <mathieu.coulibaly@reseau.sncf.fr>
2023-06-23 07:25:59 +0200

    added classe 'white-theme' on profil component'

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -14,6 +14,7 @@ export class ProfilComponent {
 
 changeTheme() {
 this.isThemeWhite = !this.isThemeWhite;
+console.log('isThemeWhite:', this.isThemeWhite);
 this.Theme.emit(this.isThemeWhite);
 }
 
