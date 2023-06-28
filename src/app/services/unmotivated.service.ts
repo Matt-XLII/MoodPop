949b8b0fda92f0f31f10e16c63b409337892fa5c
Matt-XLII <durouchet.mathieu@gmail.com>
2023-06-28 10:30:43 +0200

    updating unmotivated service

--- src/app/services/unmotivated.service.ts
+++ src/app/services/unmotivated.service.ts
@@ -20,4 +20,16 @@ export class UnmotivatedService {
     .then(response => response.json())
     .then((data: any) => console.log(this.quote = data.value));
 }
+
+fetchChuck(list:string[]):void{
+  for (let i = 0; i < 20; i++){
+  fetch("https://api.api-ninjas.com/v1/chucknorris?", {
+    method:'GET',
+    headers: {
+  'X-Api-key' : 'q80JOdvIGyOhfvSXFKWV4A==0y5PJl9FCDrqP2ik',
+  'Content-type': 'application/json',}})
+  .then(function (response) { return response.json()})
+  .then((data:any) => list.push(data.joke))
+}
+}
 }
