11f1fa4090914e6cb662aec78a01cfd704b78d42
Rricardo2431 <128594768+Rricardo2431@users.noreply.github.com>
2023-07-05 17:00:55 +0200

    modif categories

--- src/app/menu-burger/menu-burger.component.ts
+++ src/app/menu-burger/menu-burger.component.ts
@@ -33,11 +33,11 @@ toAngry(){
 }
 
 rachidJoke(){ 
-  return alert("Fuck Rachid");
+  return alert("T'as changé😧");
 }
 
 richardJoke(){
-  return alert("Fuck Richard");
+  return alert("Euh .. Sinon je fais un GIT CLONE🥴");
 }
 
 }
